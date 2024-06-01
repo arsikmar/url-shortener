a6befc064d8c5a75df99a4ba6a134427922be24b
Fantazirro <fantazirro@gmail.com>
2024-06-01 16:19:07 +0500

    Add url shortening service

---
+++ backend/UrlShortener/UrlShortenerApi/Configurations/UrlShortenerConfiguration.cs
@@ -0,0 +1,9 @@
+﻿namespace UrlShortenerApi.Configurations
+{
+    public static class UrlShortenerConfiguration
+    {
+        public const int CodeLength = 7;
+        public const string Alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        public static int AlphabetSize => Alphabet.Length;
+    }
+}
--- backend/UrlShortener/UrlShortenerApi/Services/UrlShorteningService.cs
+++ backend/UrlShortener/UrlShortenerApi/Services/UrlShorteningService.cs
@@ -1,6 +1,41 @@
-﻿namespace UrlShortenerApi.Services
+﻿using Microsoft.EntityFrameworkCore;
+using UrlShortenerApi.Configurations;
+using UrlShortenerApi.Data;
+
+namespace UrlShortenerApi.Services
 {
     public class UrlShorteningService
     {
+        private readonly Random _random;
+        private readonly ApplicationDbContext _context;
+
+        public UrlShorteningService(ApplicationDbContext context)
+        {
+            _context = context;
+            _random = new Random();
+        }
+
+        public async Task<string> GenerateUniqueCode()
+        {
+            while (true)
+            {
+                var code = GenerateCode();
+                var codeExists = await _context.ShortenedUrls.AnyAsync(u => u.Code == code);
+                if (!codeExists) return code;
+            }
+        }
+
+        private string GenerateCode()
+        {
+            var code = new char[UrlShortenerConfiguration.CodeLength];
+            
+            for (int i = 0; i < code.Length; i++)
+            {
+                var currentCharIndex = _random.Next(0, UrlShortenerConfiguration.AlphabetSize);
+                code[i] = UrlShortenerConfiguration.Alphabet[currentCharIndex];
+            }
+
+            return new string(code);
+        }
     }
 }
