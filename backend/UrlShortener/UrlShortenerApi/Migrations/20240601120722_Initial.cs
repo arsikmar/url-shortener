548d1972e7cde8254ba2c9cd3b031914220b11e0
Fantazirro <fantazirro@gmail.com>
2024-06-01 17:09:56 +0500

    Add migrations

---
+++ backend/UrlShortener/UrlShortenerApi/Migrations/20240601120722_Initial.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UrlShortenerApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "shortened_urls",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    code = table.Column<string>(type: "text", nullable: false),
+                    base_url = table.Column<string>(type: "text", nullable: false),
+                    short_url = table.Column<string>(type: "text", nullable: false),
+                    created_on_utc = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_shortened_urls", x => x.id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_shortened_urls_code",
+                table: "shortened_urls",
+                column: "code",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "shortened_urls");
+        }
+    }
+}
