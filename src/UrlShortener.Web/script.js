eef8fd48d26e86a83210baba8bb8e6fe2d44816f
Fantazirro <fantazirro@gmail.com>
2024-07-11 21:44:34 +0500

    Fix frontend issues

--- src/UrlShortener.Web/script.js
+++ src/UrlShortener.Web/script.js
@@ -1,14 +1,24 @@
 
-const apiurl = "";
+const apiurl = "http://localhost:8080/";
 
 async function submit_url() {
+    document.getElementById("load_svg").classList.toggle("d-none");
+
     var url = document.getElementById("form_text").value;
+    var request = { url: url }
 
     console.log(url);
 
     const response = await fetch(apiurl, {
-        method: "post"
+        method: "post",
+        body: JSON.stringify(request),
+        headers: {
+            'Accept': 'application/json, text/plain',
+            'Content-Type': 'application/json;charset=UTF-8'
+        }
     })
+
+    console.log(response)
 
     if (response.ok) 
     { 
@@ -17,6 +27,13 @@ async function submit_url() {
         document.getElementById("shortened_url").value = await response.json();
     } 
     else {
-        alert("Ошибка HTTP: " + response.status);
+        alert("Ошибка HTTP: " + response.statusText);
     }
+}
+
+function copy_url() {
+    var short_url = document.getElementById("shortened_url");
+    short_url.select();
+    document.execCommand("copy");
+    alert("URL скопирован в буфер обмена");
 }
